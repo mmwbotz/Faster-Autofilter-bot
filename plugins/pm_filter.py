61ce6bc431619a0f138679f1210a95c7aabd1210
MMW BOTZ <akhilkrishnausa50@gmail.com>
2025-03-07 13:52:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -737,39 +737,41 @@ async def manual_filters(client, message, text=False):
         pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
         if re.search(pattern, name, flags=re.IGNORECASE):
             reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
+
             if reply_text:
                 reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
-                if btn is not None:
-                    try:
-                        if fileid == "None":
-                            if btn == "[]":
-                                await client.send_message(group_id, reply_text, disable_web_page_preview=True)
-                            else:
-                                button = eval(btn)
-                                await client.send_message(
-                                    group_id,
-                                    reply_text,
-                                    disable_web_page_preview=True,
-                                    reply_markup=InlineKeyboardMarkup(button),
-                                    reply_to_message_id=reply_id
-                                )
-                        elif btn == "[]":
-                            await client.send_cached_media(
-                                group_id,
-                                fileid,
-                                caption=reply_text or "",
-                                reply_to_message_id=reply_id
-                            )
+
+            if btn is not None:
+                try:
+                    if fileid == "None":
+                        if btn == "[]":
+                            await client.send_message(group_id, reply_text, disable_web_page_preview=True)
                         else:
                             button = eval(btn)
-                            await message.reply_cached_media(
-                                fileid,
-                                caption=reply_text or "",
+                            await client.send_message(
+                                group_id,
+                                reply_text,
+                                disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button),
                                 reply_to_message_id=reply_id
                             )
-                    except Exception as e:
-                        logger.exception(e)
-                        break
-                return True
-    return False
+                    elif btn == "[]":
+                        await client.send_cached_media(
+                            group_id,
+                            fileid,
+                            caption=reply_text or "",
+                            reply_to_message_id=reply_id
+                        )
+                    else:
+                        button = eval(btn)
+                        await message.reply_cached_media(
+                            fileid,
+                            caption=reply_text or "",
+                            reply_markup=InlineKeyboardMarkup(button),
+                            reply_to_message_id=reply_id
+                        )
+                except Exception as e:
+                    logger.exception(e)
+                break
+    else:
+        return False
